82a806aaa2c11f693b5c41747ce2181388730d40
kagfam13 <kagfam13@htl-kaindorf.ac.at>
2017-10-30 08:25:41 +0100

    added writeCoils

--- jamodTester/src/jamodtester/readCoils/RCMaster.java
+++ jamodTester/src/jamodtester/readCoils/RCMaster.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jamodtester.readingCoils;
+package jamodtester.readCoils;
 
+import jamodtester.writeCoils.WCMaster;
 import java.awt.Color;
 import java.net.InetAddress;
 import javax.swing.JLabel;
--- jamodTester/src/jamodtester/readCoils/RCSlave.java
+++ jamodTester/src/jamodtester/readCoils/RCSlave.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jamodtester.readingCoils;
+package jamodtester.readCoils;
 
+import jamodtester.writeCoils.WCSlave;
 import net.wimpi.modbus.Modbus;
 import net.wimpi.modbus.ModbusCoupler;
 import net.wimpi.modbus.net.ModbusTCPListener;
